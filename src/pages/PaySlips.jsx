bc8ccec64b38372e7df304a6069667ca8490e05d
RAZA5500 <ytakraza@gmail.com>
2026-07-23 13:58:01 +0500

    Some

---
+++ src/pages/PaySlips.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const PaySlips = () => {
+  return (
+    <div>
+      payslips
+    </div>
+  )
+}
+
+export default PaySlips
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const setting = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default setting
